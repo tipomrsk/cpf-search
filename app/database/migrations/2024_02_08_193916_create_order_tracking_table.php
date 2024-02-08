a1350e0a643778baefeb63e814c5d2e596a39208
tipomrsk <mateusbougleuxmaia@hotmail.com>
2024-02-08 16:43:00 -0300

    chore!: order tracking migration

---
+++ app/database/migrations/2024_02_08_193916_create_order_tracking_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('order_tracking', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('order_id');
+            $table->string('status');
+            $table->time('status_date');
+            $table->timestamps();
+
+            $table->foreign('order_id')->references('uuid')->on('orders');
+        });
+
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('order_tracking');
+    }
+};
