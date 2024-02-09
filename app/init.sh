3604e42633244a628b41fd00248c874c038ddf84
tipomrsk <mateusbougleuxmaia@hotmail.com>
2024-02-09 16:44:18 -0300

    chore!: docker configs

--- app/init.sh
+++ app/init.sh
@@ -3,6 +3,19 @@
 # Instala as dependências da aplicação
 composer update
 chown -R www-data:www-data /var/www
-echo "passei por aqui"
+chmod -R 755 /var/www/storage
+chmod -R 755 /var/www/bootstrap
+
+echo "------------------------------"
+echo "-- PHP ARTISAN --"
+echo "------------------------------"
+
+php artisan storage:link
+php artisan migrate
+
+echo "------------------------------"
+echo "-- FIM PHP ARTISAN --"
+echo "------------------------------"
+
 # Inicia o PHP-FPM
 php-fpm
