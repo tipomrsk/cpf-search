35802a0c31be3160b01ab9a0b311844d1c65d2b2
tipomrsk <mateusbougleuxmaia@hotmail.com>
2024-02-09 02:38:47 -0300

    doc!:format

--- app/resources/views/components/cpf-search.blade.php
+++ app/resources/views/components/cpf-search.blade.php
@@ -1,8 +1,13 @@
 <div class="row input-group-newsletter">
     <div class="col">
-        <input class="form-control" id="cpf" type="text"
-               placeholder="000.000.000-00" aria-label="Digite seu CPF"
-               data-sb-validations="required" />
+        <input
+            class="form-control"
+            id="cpf"
+            type="text"
+            placeholder="000.000.000-00"
+            aria-label="Digite seu CPF"
+            data-sb-validations="required"
+        />
     </div>
     <div class="col-auto">
         <button class="btn btn-primary" onclick="searchOrders()">Procurar</button>
